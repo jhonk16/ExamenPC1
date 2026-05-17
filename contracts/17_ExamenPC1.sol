14e08f01f49d178ddef0993772db0ca861f3a0c0
jhonk16 <jcastrom11@upao.edu.pe>
2026-05-16 19:53:35 -0500

    Parte 1

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -1,5 +1,17 @@
 pragma solidity ^0.8.20;
 
-contract ExamenPC1 {
+import "hardhat/console.sol";
 
+contract Biblioteca000244661 {
+    struct Libro {
+        uint256 id;
+        string titulo;
+        string autor;
+    }
+
+    Libro[] public libros;
+
+    constructor() {
+        console.log("Ejecutado por: 000244661 - Jhon Kevin Castro Mendez");
+    }
 }
