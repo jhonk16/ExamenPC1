0ca6f4e52210a62359775cf6191a1b1e3c8e6a6a
jhonk16 <jcastrom11@upao.edu.pe>
2026-05-16 20:20:37 -0500

    Parte 2

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -11,7 +11,23 @@ contract Biblioteca000244661 {
 
     Libro[] public libros;
 
+    address public dirContrato = 0xd9145CCE52D386f254917e481eB44e9943F39138;
+
     constructor() {
         console.log("Ejecutado por: 000244661 - Jhon Kevin Castro Mendez");
+    }
+
+    function agregarElemento(
+        uint256 _id,
+        string memory _titulo,
+        string memory _autor
+    ) public {
+        libros.push(Libro(_id, _titulo, _autor));
+    }
+
+    function contarElementos() public returns (uint256) {
+        console.log("Cantidad de elementos:", libros.length);
+        console.log("Ejecutado por: 000244661 - Jhon Kevin Castro Mendez");
+        return libros.length;
     }
 }
