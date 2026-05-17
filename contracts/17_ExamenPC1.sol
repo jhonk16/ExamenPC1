59cf87f37b910d2a9ac9ba892d2a282ad85e2f60
jhonk16 <jcastrom11@upao.edu.pe>
2026-05-16 20:29:52 -0500

    Parte 3

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.20;
+pragma solidity >=0.8.2 <0.9.0;
 
 import "hardhat/console.sol";
 
@@ -7,27 +7,44 @@ contract Biblioteca000244661 {
         uint256 id;
         string titulo;
         string autor;
+        bool estado;
     }
 
     Libro[] public libros;
 
     address public dirContrato = 0xd9145CCE52D386f254917e481eB44e9943F39138;
 
-    constructor() {
+    modifier ejecutadoPor() {
         console.log("Ejecutado por: 000244661 - Jhon Kevin Castro Mendez");
+        _;
     }
+
+    constructor() ejecutadoPor {}
 
     function agregarElemento(
         uint256 _id,
         string memory _titulo,
-        string memory _autor
-    ) public {
-        libros.push(Libro(_id, _titulo, _autor));
+        string memory _autor,
+        bool _estado
+    ) public ejecutadoPor {
+        require(!existeId(_id), "Libro con ese ID ya existe");
+        require(bytes(_titulo).length > 0, "El titulo no puede estar vacio");
+
+        libros.push(Libro(_id, _titulo, _autor, _estado));
     }
 
-    function contarElementos() public returns (uint256) {
+    function contarElementos() public ejecutadoPor returns (uint256) {
         console.log("Cantidad de elementos:", libros.length);
-        console.log("Ejecutado por: 000244661 - Jhon Kevin Castro Mendez");
         return libros.length;
+    }
+
+    function existeId(uint256 _id) private view returns (bool) {
+        for (uint256 i = 0; i < libros.length; i++) {
+            if (libros[i].id == _id) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
